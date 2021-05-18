c9db114c022fb99c263f0048e2febfb5800e9614
Daren Nathaniel Janto <minejoynicely123@gmail.com>
2021-05-18 20:55:24 +0800

    added education

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import logo from "./logo.svg";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const ColorButton = withStyles((theme) => ({
   root: {
@@ -39,10 +43,11 @@ function App() {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <Router>
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <nav className="nav" class="bg-dark">
+          <nav className="nav" class="bg-dark fixed-top autohide">
             <div className="nav-content">
               <img src={logo} className="nav-logo" alt="Logo." />
               <Link className="links" to="/">
@@ -65,6 +70,7 @@ function App() {
           <Switch>
             <Route path="/about">
               <About />
+              <Education></Education>
             </Route>
             <Route path="/project">
               <Project />
@@ -110,7 +116,7 @@ function About() {
       <div className="aboutImage">
         <img
           className="IDphoto"
-          src="https://media-exp1.licdn.com/dms/image/C5603AQGShhMlaO38bQ/profile-displayphoto-shrink_800_800/0/1612451344342?e=1620864000&v=beta&t=TNtnKbnceUJgZk9D56hOs7eWQHzc8u2RVcyASOXXq5I"
+          src="https://media-exp1.licdn.com/dms/image/C5603AQGShhMlaO38bQ/profile-displayphoto-shrink_800_800/0/1612451344342?e=1626912000&v=beta&t=1Rhrb155E5gnZCJWketEOa6nVeD59gIWfqBPLLbofr8"
         ></img>
       </div>
       <div className="aboutText">
@@ -135,6 +141,131 @@ function About() {
   );
 }
 
+function Education() {
+  return (
+    <div className="education" data-aos="fade-up" data-aos-duration="500">
+      <div className="educationBody">
+        <div className="educationTitle">
+          <h1>Education</h1>
+          <img
+            className="line2"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Line_segment.svg/375px-Line_segment.svg.png"
+          ></img>
+        </div>
+        <div className="educationHS container-fluid justify-content-center">
+          <div>
+            <img
+              src="https://freepikpsd.com/wp-content/uploads/2019/10/logo-penabur-png-1-Transparent-Images.png"
+              className="penaburphoto"
+            ></img>
+          </div>
+          <div className="educationText">
+            <span className="schoolTitle">SMAK 6 BPK Penabur Jakarta</span>
+            <span className="schoolTitle">July 2017 - May 2020</span>
+            <img
+              src="https://dmassociates.files.wordpress.com/2014/12/thin-black-line.png?w=640"
+              style={{
+                marginTop: "-90px",
+                marginBottom: "-90px",
+                marginLeft: "-20px",
+                width: "200px",
+                borderRadius: "100%",
+              }}
+              className="img-responsive"
+            ></img>
+            <ul className="ntupenabur" style={{ marginLeft: "-40px" }}>
+              <li>Ranked 16th in 2020 Cohort</li>
+            </ul>
+          </div>
+        </div>
+        <div className="educationUni container-fluid justify-content-center">
+          <div>
+            <img
+              src="https://i.pinimg.com/originals/5f/23/6b/5f236bc3bec1c44481bc661684bec650.png"
+              className="ntuphoto"
+            ></img>
+          </div>
+          <div className="educationText">
+            <span className="schoolTitle">
+              Nanyang Technological University
+            </span>
+            <span className="schoolTitle">July 2020 - Present</span>
+            <img
+              src="https://dmassociates.files.wordpress.com/2014/12/thin-black-line.png?w=640"
+              style={{
+                marginTop: "-90px",
+                marginBottom: "-90px",
+                marginLeft: "-20px",
+                width: "200px",
+                borderRadius: "100%",
+              }}
+              className="img-responsive"
+            ></img>
+            <ul className="ntupenabur" style={{ marginLeft: "-40px" }}>
+              <li>Expected Honours (Distinction)</li>
+              <li>CGPA : 4.39/5.00</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="educationAnimate">
+        <img
+          src="https://www.ainotes.org/svg/extract.svg"
+          className="educationAnimation"
+        ></img>
+      </div>
+    </div>
+  );
+  // const [index, setIndex] = useState(0);
+
+  // const handleSelect = (selectedIndex, e) => {
+  //   setIndex(selectedIndex);
+  // };
+  // return (
+  //   <div className="education">
+  //     <div className="educationTitle">
+  //       <h1>Education</h1>
+  //     </div>
+  //     <div className="educationLine">
+  //       <img
+  //         className="line2"
+  //         src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Line_segment.svg/375px-Line_segment.svg.png"
+  //       ></img>
+  //     </div>
+  //     <div className="educationCarousel">
+  //       <Carousel activeIndex={index} onSelect={handleSelect}>
+  //         <Carousel.Item>
+  //           <img
+  //             className="d-flex w-100"
+  //             src="https://www.ntu.edu.sg/images/default-source/from-figma/ntu-placeholder-d.jpg?sfvrsn=d4050a9e_2"
+  //             alt="First slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3 className="educationText">First slide label</h3>
+  //             <p className="educationText">
+  //               Nulla vitae elit libero, a pharetra augue mollis interdum.
+  //             </p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item>
+  //         <Carousel.Item>
+  //           <img
+  //             className="w-100"
+  //             src="https://freepikpsd.com/wp-content/uploads/2019/10/logo-penabur-png-1-Transparent-Images.png"
+  //             alt="Second slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3 className="educationText">Second slide label</h3>
+  //             <p className="educationText">
+  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  //             </p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item>
+  //       </Carousel>
+  //     </div>
+  //   </div>
+  // );
+}
+
 function Project() {
   return (
     <div className="project">
@@ -146,7 +277,7 @@ function Project() {
 function Footer() {
   return (
     <div className="footer" class="fixed-bottom">
-      <ul class="icon-effect icon-effect-1a">
+      <ul className="footerIcon" class="icon-effect icon-effect-1a">
         <li>
           <a target="_blank" href="https://www.instagram.com/darenathaniel/">
             <i class="fa fa-instagram"></i>
